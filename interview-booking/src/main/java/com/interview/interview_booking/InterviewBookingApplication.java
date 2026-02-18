2ee36f3749fba071c8ecc95d5e7a0194d8951c25
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-18 20:49:19 +0530

    Initial commit

---
+++ interview-booking/src/main/java/com/interview/interview_booking/InterviewBookingApplication.java
@@ -0,0 +1,13 @@
+package com.interview.interview_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewBookingApplication.class, args);
+	}
+
+}
